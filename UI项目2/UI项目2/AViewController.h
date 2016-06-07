34e13d1e4e7c3e5d92c0a94c6d2d98feeaee95be
leikun1113 <767930911@qq.com>
2016-06-07 20:45:50 +0800

    add AVC

---
+++ UI项目2/UI项目2/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  UI项目2
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 雷坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
