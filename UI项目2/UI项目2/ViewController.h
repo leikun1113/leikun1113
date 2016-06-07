c92a50627e4c4b53d59265cadebdc015376cab7b
leikun1113 <767930911@qq.com>
2016-06-07 20:20:20 +0800

    1

---
+++ UI项目2/UI项目2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI项目2
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 雷坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI项目2/UI项目2/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  UI项目2
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 雷坤. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"雷坤");
+    NSLog(@"1111");
+    NSLog(@"2222");
+    NSLog(@"44");
+    NSLog(@"5");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
